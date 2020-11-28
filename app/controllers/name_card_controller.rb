caeb23c8b9d9d721a64246815f5d3893ad7b71b4
kymx1983 <kymx1983@gmail.com>
2020-11-28 19:23:48 +0900

    new/create作成

--- app/controllers/name_card_controller.rb
+++ app/controllers/name_card_controller.rb
@@ -1,13 +1,29 @@
 class NameCardController < ApplicationController
   def index
+    @name_card = NameCard.all
   end
 
   def show
   end
 
   def new
+
   end
 
   def edit
+  end
+
+  def create
+
+    name_Card = NameCard.new
+    name_Card.name  = params[:name]
+    name_Card.gender  = params[:gender]
+    name_Card.limit_date  = params[:birthday]
+    name_Card.address  = params[:address]
+    name_Card.tel  = params[:tel]
+
+    name_Card.save
+    redirect_to('/name_card/index')
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'name_card/show'
   get 'name_card/new'
   get 'name_card/edit'
+  post "name_card/create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
